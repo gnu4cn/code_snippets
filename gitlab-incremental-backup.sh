d69de1631a7d170b805e4a92cb675af8f8455f55
Lenny Peng <lenny.peng@senscomm.com>
2022-11-02 17:46:45 +0800

    Updated.

--- gitlab-incremental-backup.sh
+++ gitlab-incremental-backup.sh
@@ -2,6 +2,7 @@
 LOG_FILE="/var/log/gitlab_backup.log"
 COPIES_KEPT=7
 BACKUP_DIR="/mnt/gitlab-backups"
+SUBSTRING="_gitlab_backup.tar"
 
 if [[ ! -f "$LOG_FILE" ]]; then 
     touch "$LOG_FILE"
@@ -10,7 +11,9 @@ fi
 
 cd "${BACKUP_DIR}"
 
-existed_copies=`ls | grep "gitlab_backup.tar" | wc -l`
+
+
+existed_copies=`ls *${SUBSTRING} | wc -l`
 
 echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- -----执行 GitLab-jh 备份-----" >> $LOG_FILE
 echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 现有备份数：${existed_copies}" >> $LOG_FILE
@@ -19,8 +22,8 @@ if [[ $existed_copies == 0 ]]; then
     # echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 无先前备份，进行完整备份......." >> $log_file
     echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 无先前备份，进行完整备份......." >> $LOG_FILE
     echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- ----------gitlab-backup 开始运行----------" >> $LOG_FILE
-    # touch "$(date +%s)_gitlab_backup.tar"
-	/bin/gitlab-backup create >> $LOG_FILE
+    touch "$(date +%s)${SUBSTRING}"
+	# /bin/gitlab-backup create >> $LOG_FILE
 
     if [ $? -ne 0 ]; then
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份失败，退出此程序！" >> $LOG_FILE
@@ -29,19 +32,17 @@ if [[ $existed_copies == 0 ]]; then
         /bin/sync
         /bin/sleep 0.5
 
-        new_backup=`ls *.tar -t | head -n1`
+        new_backup=`ls *${SUBSTRING} -t | head -n1`
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：${new_backup}" >> $LOG_FILE
     fi
 else 
-    previous_backup=`ls *.tar -t | head -n1`
-    # last_file=$(ls -t | head -n1)
-    substring="_gitlab_backup.tar"
+    previous_backup=`ls *${SUBSTRING} -t | head -n1`
 
     # echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 发现先前备份：${previous_backup}，采取增量备份......" >> $log_file
     echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 发现先前备份：${previous_backup}，采取增量备份......" >> $LOG_FILE
     echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- ----------gitlab-backup 开始运行----------" >> $LOG_FILE
-    # touch "$(date +%s)_gitlab_backup.tar"
-	/bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$substring"} >> $LOG_FILE
+    touch "$(date +%s)${SUBSTRING}"
+	# /bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$SUBSTRING"} >> $LOG_FILE
 
     if [ $? -ne 0 ]; then
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份失败，退出此程序！" >> $LOG_FILE
@@ -50,17 +51,17 @@ else
         /bin/sync
         /bin/sleep 0.5
 
-        new_backup=`ls *.tar -t | head -n1`
+        new_backup=`ls *${SUBSTRING} -t | head -n1`
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：${new_backup}" >> $LOG_FILE
     fi
 
     /bin/sync
     /bin/sleep 1
     
-    existed_copies=`ls | grep "gitlab_backup.tar" | wc -l`
+    existed_copies=`ls *${SUBSTRING} | wc -l`
 
     while [ $existed_copies -gt $COPIES_KEPT ]; do
-        oldest_copy=`ls *.tar -t | tail -n 1`
+        oldest_copy=`ls *${SUBSTRING} -t | tail -n 1`
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 现有备份数 ${existed_copies}, 超出预设 ${COPIES_KEPT}，将删除最早备份：${oldest_copy}" >> $LOG_FILE
 
         /bin/rm -rf "${oldest_copy}"
@@ -68,7 +69,7 @@ else
         /bin/sync
         /bin/sleep 0.5
 
-        existed_copies=`ls | grep "gitlab_backup.tar" | wc -l`
+        existed_copies=`ls *${SUBSTRING} | wc -l`
     done
 fi
 
