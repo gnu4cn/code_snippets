da6a33b14d2696dd4f7c01d733340e79d311c0fd
Lenny Peng <lenny.peng@senscomm.com>
2022-11-03 10:53:36 +0800

    Updated.

--- gitlab-incremental-backup.sh
+++ gitlab-incremental-backup.sh
@@ -7,12 +7,12 @@ handle_backup_cmd_err() {
     if [ $1 -ne 0 ]; then
         echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份失败，退出此程序！" >> $LOG_FILE
         exit 1
-    else
-        /bin/sync && /bin/sleep 0.5
-
-        new_backup=`/bin/ls *${substring} -t | head -n1`
-        echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：${new_backup}" >> $LOG_FILE
     fi
+
+    /bin/sync && /bin/sleep 0.5
+
+    new_backup=`/bin/ls *${substring} -t | head -n1`
+    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：${new_backup}" >> $LOG_FILE
 }
 
 if [[ ! -f "$LOG_FILE" ]]; then touch "$LOG_FILE"; fi
@@ -31,29 +31,28 @@ if [[ $existed_copies == 0 ]]; then
     touch "$(date +%s)${substring}"
     # /bin/gitlab-backup create >> $LOG_FILE
     handle_backup_cmd_err "$?"
-else 
-    previous_backup=`/bin/ls *${substring} -t | head -n1`
-
-    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 发现先前备份：${previous_backup}，采取增量备份......" >> $LOG_FILE
-    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- ----------gitlab-backup 开始运行----------" >> $LOG_FILE
-
-    touch "$(date +%s)${substring}"
-    # /bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$substring"} >> $LOG_FILE
-    handle_backup_cmd_err "$?"
-
-    /bin/sync && /bin/sleep 1
-    
-    existed_copies=`/bin/ls *${substring} | wc -l`
-    while [ $existed_copies -gt $COPIES_KEPT ]; do
-        oldest_copy=`/bin/ls *${substring} -t | tail -n 1`
-        echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 现有备份数 ${existed_copies}, 超出预设 ${COPIES_KEPT}，将删除最早备份：${oldest_copy}" >> $LOG_FILE
-
-        /bin/rm -rf "${oldest_copy}"
-
-        /bin/sync && /bin/sleep 0.5
-
-        existed_copies=`/bin/ls *${substring} | wc -l`
-    done
+    exit 0
 fi
 
+previous_backup=`/bin/ls *${substring} -t | head -n1`
+echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 发现先前备份：${previous_backup}，采取增量备份......" >> $LOG_FILE
+echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- ----------gitlab-backup 开始运行----------" >> $LOG_FILE
+
+touch "$(date +%s)${substring}"
+# /bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$substring"} >> $LOG_FILE
+handle_backup_cmd_err "$?"
+
+/bin/sync && /bin/sleep 1
+
+existed_copies=`/bin/ls *${substring} | wc -l`
+while [ $existed_copies -gt $COPIES_KEPT ]; do
+    oldest_copy=`/bin/ls *${substring} -t | tail -n 1`
+    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 现有备份数 ${existed_copies}, 超出预设 ${COPIES_KEPT}，将删除最早备份：${oldest_copy}" >> $LOG_FILE
+
+    /bin/rm -rf "${oldest_copy}"
+
+    /bin/sync && /bin/sleep 0.5
+
+    existed_copies=`/bin/ls *${substring} | wc -l`
+done
 exit 0
