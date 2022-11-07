78198f6bdf56e3ca33f85916483d3a984bad8203
Lenny Peng <unisko@gmail.com>
2022-11-07 21:00:01 +0800

    Updated.

--- gitlab-incremental-backup.sh
+++ gitlab-incremental-backup.sh
@@ -2,6 +2,7 @@
 LOG_FILE="/var/log/gitlab_backup.log"
 COPIES_KEPT=7
 BACKUP_DIR="/mnt/gitlab-backups"
+SUBSTRING="_gitlab_backup.tar"
 
 handle_backup_cmd_err() {
     if [ $1 -ne 0 ]; then
@@ -9,7 +10,7 @@ handle_backup_cmd_err() {
     fi
 
     /bin/sync && /bin/sleep 0.5
-    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：$(/bin/ls *${substring} -t | head -n1)" >> $LOG_FILE
+    echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 备份成功：$(/bin/ls *${SUBSTRING} -t | head -n1)" >> $LOG_FILE
 }
 
 beginning_msg_log() {
@@ -31,24 +32,23 @@ fi
 if [[ ! -f "$LOG_FILE" ]]; then touch "$LOG_FILE"; fi
 
 cd "${BACKUP_DIR}"
-substring="_gitlab_backup.tar"
-if [[ $(/bin/ls *${substring} 2> /dev/null | wc -l) == 0 ]]; then
+if [[ $(/bin/ls *${SUBSTRING} 2> /dev/null | wc -l) == 0 ]]; then
     beginning_msg_log
     /bin/gitlab-backup create >> $LOG_FILE
     handle_backup_cmd_err "$?"
     exit 0
 fi
 
-previous_backup=`/bin/ls *${substring} -t | head -n1`
+previous_backup=`/bin/ls *${SUBSTRING} -t | head -n1`
 beginning_msg_log "${previous_backup}"
-/bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$substring"} >> $LOG_FILE
+/bin/gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=${previous_backup%"$SUBSTRING"} >> $LOG_FILE
 handle_backup_cmd_err "$?"
 
-existed_copies=`/bin/ls *${substring} | wc -l`
+existed_copies=`/bin/ls *${SUBSTRING} | wc -l`
 while [ $existed_copies -gt $COPIES_KEPT ]; do
-    oldest_copy=`/bin/ls *${substring} -t | tail -n 1`
+    oldest_copy=`/bin/ls *${SUBSTRING} -t | tail -n 1`
     echo "$(date '+%Y-%m-%d, %H:%M:%S %Z') -- 现有备份数 ${existed_copies}, 超出预设 ${COPIES_KEPT}，将删除最早备份：${oldest_copy}" >> $LOG_FILE
     /bin/rm -rf "${oldest_copy}" && /bin/sync && /bin/sleep 0.5
-    existed_copies=`/bin/ls *${substring} | wc -l`
+    existed_copies=`/bin/ls *${SUBSTRING} | wc -l`
 done
 exit 0
