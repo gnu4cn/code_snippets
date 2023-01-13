fa1291c85441c8e8b79fbeb159347b68ba07e4d7
Unisko PENG, <unisko@gmail.com>
2023-01-13 20:25:55 +0800

    Updated.

--- srv_mon.sh
+++ srv_mon.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 source $(dirname $0)/srv_incl.sh
 
-for name in ${!dirs[@]}; do
-    cd "$HOME/${dirs[$name]}" && npm run sl-checkout
-done
+for name in ${!dirs[@]}; do cd "$HOME/${dirs[$name]}" && npm run sl-checkout; done
 
 for name in ${!dirs[@]}; do
     resp_code=$(/usr/bin/curl -I "https://${name}.xfoss.com/sitemap.xml" 2>/dev/null | head -n 1 | cut -d$' ' -f2)
