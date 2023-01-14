4efba7149d0b0729fc77799ef0a7fb13a617879b
Unisko PENG, <unisko@gmail.com>
2023-01-14 19:36:48 +0800

    Updated.

--- custom_script.js
+++ custom_script.js
@@ -2,8 +2,3 @@ var honkit_link = $('a[class=gitbook-link]');
 
 var text = honkit_link.text();
 honkit_link.parent().html(`<span style="align: center;">${text}</span>`);
-
-$("li.chapter > a").click(function() {
-    $(".book-body").style("");
-});
-
